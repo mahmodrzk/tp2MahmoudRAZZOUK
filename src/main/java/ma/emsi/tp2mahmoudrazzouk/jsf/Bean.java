ed8c4e8f262f4c0bd1912d29aaa826691049c005
ADMIN <ADMIN@192.168.56.1>
2023-11-01 22:27:37 +0100

    ajouter de commentaires

--- src/main/java/ma/emsi/tp2mahmoudrazzouk/jsf/Bean.java
+++ src/main/java/ma/emsi/tp2mahmoudrazzouk/jsf/Bean.java
@@ -1,35 +1,35 @@
-package ma.emsi.tp2mahmoudrazzouk.jsf;
+package ma.emsi.tp2mahmoudrazzouk.jsf; // Déclaration du package
 
 import java.util.ArrayList;
 import java.util.List;
 import jakarta.inject.Named;
 import jakarta.enterprise.context.RequestScoped;
 
-@Named(value = "bean")
-@RequestScoped
+@Named(value = "bean") // Annotation indiquant que cet objet est géré par CDI (Contexts and Dependency Injection) avec le nom "bean"
+@RequestScoped // Portée de l'objet bean (RequestScoped signifie que l'objet est créé pour chaque requête)
+
 public class Bean {
 
-    private int nombre;
+    private int nombre; // Déclaration de la variable d'instance "nombre"
 
-    public int getNombre() {
+    public int getNombre() { // Méthode pour obtenir la valeur de "nombre"
         return nombre;
     }
 
-    public void setNombre(int nombre) {
+    public void setNombre(int nombre) { // Méthode pour définir la valeur de "nombre"
         this.nombre = nombre;
     }
 
-    public List<Integer> getNombresSuivants() {
-        int nb = 5;
-        List<Integer> l = new ArrayList<>(nb);
-        for (int i = nombre; i < nombre + nb; i++) {
-            l.add(i);
+    public List<Integer> getNombresSuivants() { // Méthode pour obtenir une liste de nombres suivants
+        int nb = 5; // Nombre de nombres suivants à générer
+        List<Integer> l = new ArrayList<>(nb); // Création d'une liste d'entiers
+        for (int i = nombre; i < nombre + nb; i++) { // Boucle pour générer les nombres suivants
+            l.add(i); // Ajout du nombre à la liste
         }
-        return l;
+        return l; // Retourne la liste des nombres suivants
     }
 
-    public String afficher() {
-        return "affichage_3?nb=" + nombre + "&amp;faces-redirect=true";
+    public String afficher() { // Méthode pour rediriger vers une autre page
+        return "affichage_3?nb=" + nombre + "&amp;faces-redirect=true"; // Retourne l'URL de redirection
     }
-
 }
--- src/main/java/ma/emsi/tp2mahmoudrazzouk/jsf/Bean1.java
+++ src/main/java/ma/emsi/tp2mahmoudrazzouk/jsf/Bean1.java
@@ -1,35 +1,35 @@
-package ma.emsi.tp2mahmoudrazzouk.jsf;
+package ma.emsi.tp2mahmoudrazzouk.jsf; // Déclaration du package
 
 import java.util.ArrayList;
 import java.util.List;
 import jakarta.inject.Named;
 import jakarta.enterprise.context.RequestScoped;
 
-@Named(value = "bean2")
-@RequestScoped
-public class Bean1 {
+@Named(value = "bean2") // Annotation indiquant que cet objet est géré par CDI (Contexts and Dependency Injection) avec le nom "bean2"
+@RequestScoped // Portée de l'objet bean2 (RequestScoped signifie que l'objet est créé pour chaque requête)
 
-    private int nombre;
+public class Bean1 { // Classe Bean1 (nom de la classe peut être différent du nom de l'objet)
 
-    public int getNombre() {
+    private int nombre; // Déclaration de la variable d'instance "nombre"
+
+    public int getNombre() { // Méthode pour obtenir la valeur de "nombre"
         return nombre;
     }
 
-    public void setNombre(int nombre) {
+    public void setNombre(int nombre) { // Méthode pour définir la valeur de "nombre"
         this.nombre = nombre;
     }
 
-    public List<Integer> getNombresSuivants() {
-        int nb = 5;
-        List<Integer> l = new ArrayList<>(nb);
-        for (int i = nombre; i < nombre + nb; i++) {
-            l.add(i);
+    public List<Integer> getNombresSuivants() { // Méthode pour obtenir une liste de nombres suivants
+        int nb = 5; // Nombre de nombres suivants à générer
+        List<Integer> l = new ArrayList<>(nb); // Création d'une liste d'entiers
+        for (int i = nombre; i < nombre + nb; i++) { // Boucle pour générer les nombres suivants
+            l.add(i); // Ajout du nombre à la liste
         }
-        return l;
+        return l; // Retourne la liste des nombres suivants
     }
 
-    public String afficher() {
-        return "affichage_4?nb=" + nombre + "&amp;faces-redirect=true";
+    public String afficher() { // Méthode pour rediriger vers une autre page
+        return "affichage_4?nb=" + nombre + "&amp;faces-redirect=true"; // Retourne l'URL de redirection
     }
-
 }
